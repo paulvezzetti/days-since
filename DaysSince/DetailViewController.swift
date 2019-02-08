5f63c85f92a2a70f1a6e885e4ffaf76f55b25e9d
Paul Vezzetti <Paul.Vezzetti@sas.com>
2019-02-08 14:25:28 -0500

    Initial Commit

---
+++ DaysSince/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  DaysSince
+//
+//  Created by Paul Vezzetti on 2/8/19.
+//  Copyright © 2019 Paul Vezzetti. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
