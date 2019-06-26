430b138fa19f1d8a10c4ac0fdf3082b5d37b3b6c
Paul Vezzetti <Paul.Vezzetti@sas.com>
2019-06-26 14:51:14 -0400

    Clean up

--- DaysSince/src/view/HintPopoverViewController.swift
+++ DaysSince/src/view/HintPopoverViewController.swift
@@ -12,24 +12,8 @@ class HintPopoverViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        let label:UILabel = UILabel(frame: CGRect(x: 25, y: 0, width: 500, height: 154))
-//        label.text = "Click the + to add a new activity."
-//        label.font = UIFont(name: "Avenir", size: 16.0)
-//        //let view = UIView()
-//        self.view.addSubview(label)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
